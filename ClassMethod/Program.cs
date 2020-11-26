923067f978107139faf0538a74c9ee6cbf40275a
Rev1ved <Rev1ved911@gmail.com>
2020-11-26 15:41:12 +0700

    ClassMethod complete

--- ClassMethod/Program.cs
+++ ClassMethod/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+Эберлинг Виктор Викторович
+
+6. *Создать класс с методами, которые могут пригодиться в вашей учебе (Print, Pause).
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +12,28 @@ using System.Threading.Tasks;
 
 namespace ClassMethod
 {
-    class Program
+    class Relief
     {
+        static void Print(string Message)
+        {
+            Console.Write(Message);
+        }
+        static void Pause()
+        {
+            Print("Press eny key...");
+            Console.ReadKey();
+        }
         static void Main(string[] args)
         {
+            #region Настройки консоли
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.SetWindowSize(17, 2);
+            Console.SetBufferSize(17, 2);
+            #endregion
+            Print("Hello!\n");
+            Pause();
         }
     }
 }
