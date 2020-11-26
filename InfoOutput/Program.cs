f58546f770927a78cb3d4ebbbc9dffc8808f8765
Rev1ved <Rev1ved911@gmail.com>
2020-11-26 15:16:09 +0700

    Start InfoOutput

---
+++ InfoOutput/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Эберлинг Виктор Викторовчи
+
+5.
+а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.
+б) Сделать задание, только вывод организуйте в центре экрана
+в) *Сделать задание б с использованием собственных методов (например, Print(string ms, int x,int y)
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Настройки консоли
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            #endregion
+
+        }
+    }
+}
